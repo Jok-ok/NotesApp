308bcb5d65b6d3ca148fd47823d4ceaeac0c4afd
Alexandr Vorobey <nujniy.vorobey@gmail.com>
2024-01-26 14:10:07 +0300

    Initial Commit

---
+++ NotesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotesApp
+//
+//  Created by Александр Воробей on 26.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
